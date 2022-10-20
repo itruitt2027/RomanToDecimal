d62c096d42e58bcd54892d2b770c073b44747e5e
itruitt2027 <itruitt2027@gmail.com>
2022-10-20 16:35:57 -0400

    I think this will work

---
+++ src/RomanToDecimalTest.java
@@ -0,0 +1,9 @@
+
+
+public class RomanToDecimalTest {
+    public static void main(String[] args) {
+        String oldStr = "ABCDEF";
+        String newStr = oldStr.substring(1, 3) + oldStr.substring(4);
+        System.out.println(newStr);
+    }
+}
