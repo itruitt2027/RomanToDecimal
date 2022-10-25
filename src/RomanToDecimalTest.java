5d5d9f2591762e3500a5325da8582190cac02fcb
itruitt2027 <itruitt2027@gmail.com>
2022-10-24 22:28:06 -0400

    New stuff

--- src/RomanToDecimalTest.java
+++ src/RomanToDecimalTest.java
@@ -1,43 +1,8 @@
-import static org.junit.Assert.*;
-/**
- * A junit test for testing different types of user entry. (Emiliano showed me what JUnit tests are.)
- */
-public class RomanToDecimalTest {
 
-    @org.junit.Test
-    /*
-      Putting in the tests for each of the values that are logical and non-logical.
-     */
-    public void romanToDecimal() {
-        assertEquals(RomanToDecimal.romanToDecimal("I"),1);
-        assertEquals(RomanToDecimal.romanToDecimal("II"),2);
-        assertEquals(RomanToDecimal.romanToDecimal("III"),3);
-        assertEquals(RomanToDecimal.romanToDecimal("IV"),4);
-        assertEquals(RomanToDecimal.romanToDecimal("V"),5);
-        assertEquals(RomanToDecimal.romanToDecimal("X"),10);
-        assertEquals(RomanToDecimal.romanToDecimal("XV"),15);
-        assertEquals(RomanToDecimal.romanToDecimal("XVI"),16);
-        assertEquals(RomanToDecimal.romanToDecimal("XVII"),17);
-        assertEquals(RomanToDecimal.romanToDecimal("XVIII"),18);
-        assertEquals(RomanToDecimal.romanToDecimal("XX"),20);
-        assertEquals(RomanToDecimal.romanToDecimal("XI"),11);
-        assertEquals(RomanToDecimal.romanToDecimal("XII"),12);
-        assertEquals(RomanToDecimal.romanToDecimal("XV"),25);
-        assertEquals(RomanToDecimal.romanToDecimal("XXX"),30);
-        assertEquals(RomanToDecimal.romanToDecimal("ur mom"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("NO"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("potato"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("goblin"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("IXHAHA"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("poop"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("coding"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("0102012"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("COOL"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("WOW"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("VOOBER"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("Basketball"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("Soccer"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("WaterPolo"),-1);
-        assertEquals(RomanToDecimal.romanToDecimal("Tennis"),-1);
-        }
+public class RomanToDecimalTest {
+    public static void main(String[] args) {
+        String oldStr = "ABCDEF";
+        String newStr = oldStr.substring(1, 3) + oldStr.substring(4);
+        System.out.println(newStr);
+    }
 }
---
+++ src/whiteBoxtest.java
@@ -0,0 +1,45 @@
+
+    import static org.junit.Assert.*;
+    /**
+     * A junit test for testing different types of user entry. (Emiliano showed me what JUnit tests are.)
+     */
+    public class whiteBoxtest {
+
+        @org.junit.Test
+    /*
+      Putting in the tests for each of the values that are logical and non-logical.
+     */
+        public static void main(String[] args) {
+            assertEquals(RomanToDecimal.romanToDecimal("I"),1);
+            assertEquals(RomanToDecimal.romanToDecimal("II"),2);
+            assertEquals(RomanToDecimal.romanToDecimal("III"),3);
+            assertEquals(RomanToDecimal.romanToDecimal("IV"),4);
+            assertEquals(RomanToDecimal.romanToDecimal("V"),5);
+            assertEquals(RomanToDecimal.romanToDecimal("X"),10);
+            assertEquals(RomanToDecimal.romanToDecimal("XV"),15);
+            assertEquals(RomanToDecimal.romanToDecimal("XVI"),16);
+            assertEquals(RomanToDecimal.romanToDecimal("XVII"),17);
+            assertEquals(RomanToDecimal.romanToDecimal("XVIII"),18);
+            assertEquals(RomanToDecimal.romanToDecimal("XX"),20);
+            assertEquals(RomanToDecimal.romanToDecimal("XI"),11);
+            assertEquals(RomanToDecimal.romanToDecimal("XII"),12);
+            assertEquals(RomanToDecimal.romanToDecimal("XXV"),25);
+            assertEquals(RomanToDecimal.romanToDecimal("XXX"),30);
+            assertEquals(RomanToDecimal.romanToDecimal("ur mom"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("NO"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("potato"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("goblin"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("IXHAHA"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("poop"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("coding"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("0102012"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("COOL"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("WOW"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("VOOBER"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("Basketball"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("Soccer"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("WaterPolo"),-1);
+            assertEquals(RomanToDecimal.romanToDecimal("Tennis"),-1);
+        }
+    }
+
